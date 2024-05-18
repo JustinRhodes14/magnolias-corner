394886a600f0dfc56f4ec4e45f1eb4a4abf0b974
JustinRhodes14 <rhodesju21@gmail.com>
2024-05-18 13:48:33 -0400

    feat: better banner, centered items, and other css stuff

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/esm/Image';
+
 import Header from './components/Header';
 import Gallery from './components/home/Gallery';
 import Sidebar from './components/home/Sidebar';
 import About from './pages/About';
+import Contact from './pages/Contact';
+import Blog from './pages/Blog';
+import magnoliaBanner from '/Users/rhodes/Projects/magnoliasCorner/magnolias-corner/src/pictures/magnolia-banner.png';
 
 // GOAL: Vogue fashion magazine displaying concepts, fashion shows, 
 // and general discussion around fashion
@@ -17,28 +22,38 @@ import About from './pages/About';
 function App() {
   return (
     <div>
-    <Header />
-    <Container fluid>
+      <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=GFS+Didot&display=swap" rel="stylesheet" />
       <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-        crossorigin="anonymous" 
-      />
-      <Row>
-      <Col sm={3}/>
-      <Col sm={5} className='p-4'>
-        <Gallery />
-      </Col>
-      <Col sm={1}/>
-      <Col sm={3}>
-        <Sidebar />
-      </Col>
-      </Row>
-    </Container>
-    <br />
-    <br />
-    <About />
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossorigin="anonymous" 
+        />
+      </head>
+      <Image src={magnoliaBanner} fluid></Image>
+      <Header />
+      <Container fluid>
+        <Row>
+        <Col sm={3}/>
+        <Col sm={5} className='p-4'>
+          <Gallery />
+        </Col>
+        <Col sm={1}/>
+        <Col sm={3}>
+          <Sidebar />
+        </Col>
+        </Row>
+      </Container>
+      <br />
+      <br />
+      <About />
+      <br />
+      <br />
+      <Contact />
+      <Blog />
     </div>
   );
 }
---
+++ src/pages/Contact.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+
+
+function Contact() {
+  return (
+        <Form>
+            <div className='Form-header'>
+                <h1>Commisions, Conversation, Questions?</h1>
+                <p>Commision requests? Inquiry for work? Questions about my process? Feel free to reach out below!</p>
+                <p>When reaching out, please be sure to include your name, and I'll get back to you as soon as possible!</p>
+                <p>Even if you don't necessarily have a question, I'm always open to chat <span role='img' aria-label='Smile Emoji'>🙂</span></p>
+            </div>
+            <Row>
+                <Col sm={3}/>
+                <Col sm={6}>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control as="textarea" rows={5} />
+                    </Form.Group>
+                </Col>
+                <Col sm={3}/>
+            </Row>
+        </Form>
+  );
+}
+
+export default Contact;
